f2d7322d442049bc7104f9d05e9ad769fa903a3c
Abraão Ulhoa <79339769+abraaoulhoa@users.noreply.github.com>
2025-08-07 15:28:59 -0300

    vite config certo

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/potifolio/',
+  base:'/potfolio/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
